334227b492be085ca8e09afad1520f896b9b3ce3
Shahid <shahid.sarker@gmail.com>
2020-06-03 11:33:40 -0400

    FormTest

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
-import ContactCard from "./ContactCard";
+import FormTest from "./FormTest";
 import Decrement from "./Decrement";
 import "./App.css";
-
-class EventExample extends Component {
-  save() {
-    alert("clicked save!");
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.save}>Save</button>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   render() {
@@ -23,7 +10,7 @@ class App extends Component {
         <Decrement start={5} />
         <Decrement start={15} />
         <Decrement start={50} />
-        <EventExample />
+        <FormTest />
       </>
     );
   }
